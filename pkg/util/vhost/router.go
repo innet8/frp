d075b7de24f3b37a00035ffa6a4aade473db001f
aabb <aa@bb.cc>
2021-03-22 18:33:26 +0800

    新增错误日志

--- pkg/util/vhost/router.go
+++ pkg/util/vhost/router.go
@@ -2,6 +2,7 @@ package vhost
 
 import (
 	"errors"
+	"github.com/fatedier/frp/pkg/util/log"
 	"sort"
 	"strings"
 	"sync"
@@ -34,6 +35,7 @@ func (r *Routers) Add(domain, location string, payload interface{}) error {
 	defer r.mutex.Unlock()
 
 	if _, exist := r.exist(domain, location); exist {
+		log.Info("error domain [%s], location [%s]", domain, location)
 		return ErrRouterConfigConflict
 	}
 
