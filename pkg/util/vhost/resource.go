4d44e7d784b1f5ce6780957f7451520772e5da4f
aabb <aa@bb.cc>
2021-03-24 14:53:50 +0800

    404页面改成json

--- pkg/util/vhost/resource.go
+++ pkg/util/vhost/resource.go
@@ -28,27 +28,7 @@ var (
 )
 
 const (
-	NotFound = `<!DOCTYPE html>
-<html>
-<head>
-<title>Not Found</title>
-<style>
-    body {
-        width: 35em;
-        margin: 0 auto;
-        font-family: Tahoma, Verdana, Arial, sans-serif;
-    }
-</style>
-</head>
-<body>
-<h1>The page you requested was not found.</h1>
-<p>Sorry, the page you are looking for is currently unavailable.<br/>
-Please try again later.</p>
-<p>The server is powered by <a href="https://github.com/fatedier/frp">frp</a>.</p>
-<p><em>Faithfully yours, frp.</em></p>
-</body>
-</html>
-`
+	NotFound = `{"ret":-404,"msg":"The page you requested was not found.","data":{}}`
 )
 
 func getNotFoundPageContent() []byte {
@@ -71,14 +51,14 @@ func getNotFoundPageContent() []byte {
 func notFoundResponse() *http.Response {
 	header := make(http.Header)
 	header.Set("server", "frp/"+version.Full())
-	header.Set("Content-Type", "text/html")
+	header.Set("Content-Type", "application/json")
 
 	res := &http.Response{
-		Status:     "Not Found",
-		StatusCode: 404,
-		Proto:      "HTTP/1.0",
+		Status:     "OK",
+		StatusCode: 200,
+		Proto:      "HTTP/1.1",
 		ProtoMajor: 1,
-		ProtoMinor: 0,
+		ProtoMinor: 1,
 		Header:     header,
 		Body:       ioutil.NopCloser(bytes.NewReader(getNotFoundPageContent())),
 	}
